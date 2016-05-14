eaaf4bf478a45cf15455743f3cfcd54077ca24eb
Cameron Smith <spellbee23@camerons-macbook-pro.local>
2016-05-14 15:53:36 -0600

    Initial Commit

---
+++ LoL Champ DPS/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LoL Champ DPS
+//
+//  Created by Cameron Smith on 5/14/16.
+//  Copyright © 2016 Cameron Smith. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
